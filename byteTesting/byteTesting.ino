530dbd7fa75499bcf1d9897517d9a43776e7c077
Sahil Mujadedi <sahilmujadedi@pop-os.localdomain>
2023-01-15 18:34:27 -0700

    fixed numbers above 19 outputting incorrect byte

--- byteTesting/byteTesting.ino
+++ byteTesting/byteTesting.ino
@@ -1,24 +1,26 @@
-byte input = 14;
-
 void setup() {
 	Serial.begin(9600);
+  Serial.println(BCDAlgo(57));
 }
 
 void loop() {
-	BCDAlgo(input);
-	delay(5000);
-	input ++;
+
 }
-void BCDAlgo(byte inputByte) {
+byte BCDAlgo(byte inputByte) {
 	byte LSNibble = 0;
 	byte outputByte = 0;
+
 	for (int i = 0; i < 8; i++) {
 		if (LSNibble >= 5) {
 			outputByte += 3;
 		}
+
 		outputByte = outputByte << 1;
 		outputByte += bitRead(inputByte, 7 - i);
-		LSNibble = (outputByte << 4) >> 4;
+
+    for (int j = 0; j < 4; j++) {
+      bitWrite(LSNibble, j, bitRead(outputByte, j));
+    }
 	}
-	Serial.println(outputByte);
+  return outputByte;
 }
